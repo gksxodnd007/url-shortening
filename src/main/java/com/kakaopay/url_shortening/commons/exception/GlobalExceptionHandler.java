10becddb14f771feec14cd8ace52d414ba9b9237
codingsquid <gksxodnd007@gmail.com>
2018-06-13 10:51:20 +0900

    fix: 모든 에러 RuntimeException으로만 처리

--- src/main/java/com/kakaopay/url_shortening/commons/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kakaopay/url_shortening/commons/exception/GlobalExceptionHandler.java
@@ -1,13 +1,18 @@
 package com.kakaopay.url_shortening.commons.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({ BaseException.class, RuntimeException.class })
-    public String handleException(BaseException e) {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler(RuntimeException.class)
+    public String handleException(RuntimeException e) {
+        logger.debug("msg : {}, cause : {}", e.getMessage(), e.getCause());
         return "error";
     }
 
