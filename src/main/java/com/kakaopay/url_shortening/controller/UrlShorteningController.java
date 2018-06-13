d49c745f423c0f0d3f8fb0e293753eb1da88f94a
codingsquid <gksxodnd007@gmail.com>
2018-06-13 11:04:21 +0900

    feat: url의 마지막 문자가 '/'일시 '/'를 제거 & 변경된 메서드 적용

--- src/main/java/com/kakaopay/url_shortening/controller/UrlShorteningController.java
+++ src/main/java/com/kakaopay/url_shortening/controller/UrlShorteningController.java
@@ -1,10 +1,10 @@
 package com.kakaopay.url_shortening.controller;
 
-import com.kakaopay.url_shortening.commons.exception.BaseException;
-import com.kakaopay.url_shortening.commons.model.BaseCode;
 import com.kakaopay.url_shortening.domain.ShortenedUrlDomain;
 import com.kakaopay.url_shortening.service.UrlShorteningService;
 import org.apache.commons.validator.routines.UrlValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -21,6 +21,9 @@ public class UrlShorteningController {
 
     private final UrlShorteningService urlShorteningService;
     private final UrlValidator urlValidator;
+    private static final String HOST = "http://localhost:8080/";
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public UrlShorteningController(@Qualifier("urlShorteningServiceImpl") UrlShorteningService urlShorteningService) {
@@ -32,13 +35,14 @@ public class UrlShorteningController {
     public String shorteningUrl(@RequestParam("targetUrl") String url,
                                 Map<String, String> model) {
         if (!urlValidator.isValid(url)) {
+            logger.info("msg: {}, targetUrl : {}", "유효하지 않은 URL요청", url);
             model.put("isValidUrl", "잘못된 URL입니다. 다시 입력해주세요");
             return "index";
         }
 
-        Optional<ShortenedUrlDomain> domain = urlShorteningService.getShortenedUrl(url);
+        Optional<ShortenedUrlDomain> domain = urlShorteningService.getShortenedUrlKey(removeEndDelimiter(url));
         domain.ifPresent(elem -> {
-            model.put("shortenedUrl", elem.getShortenedUrl());
+            model.put("shortenedUrl", HOST + elem.getShortenedUrlKey());
             model.put("originalUrl", elem.getOriginalUrl());
         });
         return "index";
@@ -49,6 +53,15 @@ public class UrlShorteningController {
         final Optional<ShortenedUrlDomain> url = urlShorteningService.getOriginalUrl("http://localhost:8080" + request.getRequestURI());
 
         return url.map(elem -> "redirect:" + elem.getOriginalUrl())
-                .orElseThrow(BaseException::new);
+                .orElseThrow(RuntimeException::new);
     }
+
+    private String removeEndDelimiter(String url) {
+        if (url.endsWith("/")) {
+            return url.substring(0, url.length() - 1);
+        } else {
+            return url;
+        }
+    }
+
 }
